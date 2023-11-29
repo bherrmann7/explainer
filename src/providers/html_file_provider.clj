346ff4385531f448dee1e68a1e34164dfda00c8c
bob <bob@jadn.com>
2023-11-29 15:55:49 -0500

    add more error handling into the :html-file provider

--- src/providers/html_file_provider.clj
+++ src/providers/html_file_provider.clj
@@ -2,22 +2,31 @@
 (ns providers.html-file-provider
   (:require [providers.chunk-provider :refer [ChunkProviderProtocol]]
             [selmer.parser]
+            [clojure.java.io]
             [utils]))
 
 (defn compute-input-filename [context data-map]
   (let [{:keys [input-dir]} context]
     (str input-dir "/" (:filename data-map))))
 
+(defn error [ msg ]
+   (println "Yikes: " msg)
+   (str "<h1 style='background-color: red; color: white;'>" msg "</h1>"))
+
 (defn create-html [context data-map]
-  (selmer.parser/render (slurp (compute-input-filename context data-map)) data-map))
+  (cond
+    (not (map? data-map)) (error "The data provided to the :html-file chunk must be a map.  It must have a key :filename")
+    (not (contains? data-map :filename)) (error "The map provided to the :html-file chunk, must provide a :filename key")
+    :else
+    (let [filename (compute-input-filename context data-map)]
+      (if (not (.exists (clojure.java.io/file filename)))
+        (error (str "Missing file: " filename))
+        (selmer.parser/render (slurp filename) data-map)))))
 
 (defn is-dirty
   "If the html-file source file has changed, rebuild the output file"
   [context data-map output-filename]
-  (let
-   [{:keys [input-dir]} context
-    input-filename (str input-dir "/" (:filename data-map))]
-    (utils/is-newer input-filename output-filename)))
+  (utils/is-newer (compute-input-filename context data-map) output-filename))
 
 (deftype Provider [context data-map output-filename]
   ChunkProviderProtocol
